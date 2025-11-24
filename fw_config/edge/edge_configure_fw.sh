9553e47e446f5bea13f05d7c477296eefad4dee1
supermalzi <robin-malzbender@web.de>
2025-11-24 13:31:57 +0100

    Moved nftables conf to config/firewalls and mounting conf instead of using sh

--- fw_config/edge/edge_configure_fw.sh
+++ fw_config/edge/edge_configure_fw.sh
@@ -25,17 +25,17 @@ $NFT add rule inet filter FORWARD iifname "ethwan" oifname "ethdmz" \
 # F(2,5,7,14,15) Internal Router (ALL Subnets) (10.10.0.0/16) -> Internet HTTP/HTTPS via ethwan -> ethdmz
 $NFT add rule inet filter FORWARD iifname "ethtransit" oifname "ethwan" \
     ip saddr 10.10.0.0/16 meta l4proto { tcp, udp } th dport { 80, 443 } \
-    log prefix "FWE_ALLOW_INTERNEL_TO_INTERNET: " counter accept
+    log prefix \"FWE_ALLOW_INTERNEL_TO_INTERNET: \" counter accept
 
 # F34 DNS Time Server -> Internet HTTP/HTTPS via ethwan -> ethdmz
 $NFT add rule inet filter FORWARD iifname "ethtransit" oifname "ethwan" \
     ip saddr 10.10.30.4 udp dport { 53, 123 } \
-    log prefix "FWE_ALLOW_INTERNEL_DNS: " counter accept
+    log prefix \"FWE_ALLOW_INTERNEL_DNS: \" counter accept
 
 # F(TBD) Edge Router (10.10.50.1) -> Internet HTTP/HTTPS OUTBOUND -> ethwan
 $NFT add rule inet filter OUTPUT oifname "ethwan" \
     meta l4proto { tcp, udp } th dport {80,443} \
-    log prefix "FWI_ALLOW_EAGE_ROUTER_INTERNET: " counter accept
+    log prefix \"FWI_ALLOW_EAGE_ROUTER_INTERNET: \" counter accept
 
 # 3. Explizites 'Log & Drop' am Ende
-$NFT add rule inet filter FORWARD log prefix \"FWE_DENIED_FWD: \" drop
+$NFT add rule inet filter FORWARD log prefix "FWE_DENIED_FWD: " drop
